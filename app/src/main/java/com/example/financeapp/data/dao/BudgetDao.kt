b73b352620b9467bde9845c9b1dfe5d3a16f9e0b
Neider-dev483 <neidervalbuena@825gmail.com>
2026-04-30 00:22:39 -0500

    feat(dao): Created class BudgetDao and SharedExpenseDao

---
+++ app/src/main/java/com/example/financeapp/data/dao/BudgetDao.kt
@@ -0,0 +1,36 @@
+package com.example.financeapp.data.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.financeapp.data.model.Budget
+import com.example.financeapp.data.model.Expense
+import com.example.financeapp.data.model.ExpenseCategory
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
+interface BudgetDao {
+    // 1. Insertar presupuesto
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(budget: Budget)
+
+    // 2. Obtener presupuestos por mes y año
+    @Query("SELECT * FROM budgets  WHERE userId = :userId AND month = :month AND year = :year")
+    fun getBudgetsByMonthYear(userId: Long, month: Int, year: Int): Flow<List<Budget>>
+
+    // 3. Obtener un presupuesto específico
+    @Query("SELECT * FROM budgets WHERE userId = :userId AND category = :category AND month = :month AND year = :year LIMIT 1")
+    fun getBudgetByCategoryMonthYear(userId: Long, category: ExpenseCategory, month: Int, year: Int): Flow<Budget?>
+
+    // 4. Actualizar
+    @Update
+    suspend fun update(budget: Budget)
+
+    // 5. Eliminar
+    @Delete
+    suspend fun delete(budget: Budget)
+}
--- app/src/main/java/com/example/financeapp/data/dao/ExpenseDao.kt
+++ app/src/main/java/com/example/financeapp/data/dao/ExpenseDao.kt
@@ -1,4 +1,45 @@
 package com.example.financeapp.data.dao
 
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.financeapp.data.model.Expense
+import com.example.financeapp.data.model.ExpenseCategory
+import kotlinx.coroutines.flow.Flow
+
+
+@Dao
 interface ExpenseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(expense: Expense)
+
+    @Query("SELECT * FROM expenses WHERE userId= :userId ORDER BY date DESC")
+    fun getExpensesByUser(userId: Long): Flow<List<Expense>>
+
+
+    @Query("SELECT * FROM expenses WHERE userId = :userId AND category = :category ORDER BY date DESC")
+    fun getExpensesByCategory(userId: Long, category: ExpenseCategory): Flow<List<Expense>>
+
+    @Query("SELECT * FROM expenses WHERE userId = :userId AND date BETWEEN :starDate AND :endDate ORDER BY date DESC")
+    fun getExpenseByDateRange(userId: Long, starDate: Long, endDate: Long): Flow<List<Expense>>
+
+    @Query("SELECT SUM(amount) FROM expenses WHERE userId = :userId AND date BETWEEN :starDate AND :endDate")
+    fun getTotalExpenseByDateRange(userId: Long, starDate: Long, endDate: Long): Flow<Double>
+
+    @Query("SELECT SUM(amount) FROM expenses WHERE userId = :userId AND category= :category AND date BETWEEN :startDate AND :endDAte")
+    fun getTotalExpensesByCategoryAndDateRange(
+        userId: Long,
+        category: ExpenseCategory,
+        starDate: Long,
+        endDate: Long
+    ): Flow<Double>
+
+    @Update
+    suspend fun update(expense: Expense)
+
+    @Delete
+    suspend fun delete(expense: Expense)
 }

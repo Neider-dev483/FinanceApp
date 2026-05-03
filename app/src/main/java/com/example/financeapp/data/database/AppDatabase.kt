082bb74a8b61a90afab82e259f1768b893e98c66
Neider-dev483 <neidervalbuena@825gmail.com>
2026-05-03 12:02:36 -0500

    feat(databse): created db, file AppDatabbase and Converters  in feature/data-database

--- app/src/main/java/com/example/financeapp/data/database/AppDatabase.kt
+++ app/src/main/java/com/example/financeapp/data/database/AppDatabase.kt
@@ -3,7 +3,6 @@ package com.example.financeapp.data.database
 import android.content.Context
 import androidx.room.Database
 
-)
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters

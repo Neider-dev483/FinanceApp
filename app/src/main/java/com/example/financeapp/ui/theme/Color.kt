3a843b6de17121d199e572306a324a8079ce59a4
Neider-dev483 <neidervalbuena@825gmail.com>
2026-05-06 20:27:58 -0500

    feat: created theme and color in the file ui

--- app/src/main/java/com/example/financeapp/ui/theme/Color.kt
+++ app/src/main/java/com/example/financeapp/ui/theme/Color.kt
@@ -1,11 +1,47 @@
 package com.example.financeapp.ui.theme
 
+import android.icu.text.IDNA
+import android.text.style.BackgroundColorSpan
 import androidx.compose.ui.graphics.Color
+import androidx.room.PrimaryKey
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
+
+val PurpleGrey80 = Color(0xFFD0BCFF)
 val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
+val Purple40 = Color(0xFF6650A4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
+
+//Colores Principales - Temas financieros
+
+val PrimaryGreen = Color(0xff00c853)
+val PrimaryGreenDark = Color(0XFF00A043)
+val SecondaryBlue = Color(0xFF2196F3)
+val AccentOrange = Color(0xFFFF9800)
+
+//Colores de fondo
+
+val BackgroundLight = Color(0xFFF6F8FA)
+val SurfaceLigth = Color(0xFFFFFFFFFF)
+val BackgroundDark = Color(0xFF121212)
+val SurfaceDark = Color(0xFF1E1E1E)
+
+//Colores de estado
+
+val SuccesGreen = Color(0xFF4CAF50)
+val ErrorRed = Color(0xFFF44336)
+val WarningYellow = Color(0xFFFFC107)
+val InfoBlue = Color(0xFF2196F3)
+
+// Colores de categoria
+
+val CategoryFood = Color(0xFFFF6B6B)
+val CategoryTransport = Color(0xFF4ECDC4)
+val CategoryEntertaiment = Color(0xFFFFBE0B)
+val CategoryBills = Color(0xFF8C5CC6)
+val CategoryShoppin = Color(0xFFEC4899)
+val CategoryHealt = Color(0xFF10B981)
+val CategoryEducation = Color(0xFF3B82F6)
+val CategoryTravel = Color(0xFFF59E0B)
+val CategoryOther = Color(0xFF6B7280)
